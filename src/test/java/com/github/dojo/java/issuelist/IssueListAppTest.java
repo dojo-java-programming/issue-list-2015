7da8cc0955ea4e45b01f48bc910a5204c69e8992
Gerlof Vriesinga <gerlof.vriesinga@duo.nl>
2016-02-25 19:13:26 +0100

    Fixes test cases, to be inline with new IssueListApp implementation

--- src/test/java/com/github/dojo/java/issuelist/IssueListAppTest.java
+++ src/test/java/com/github/dojo/java/issuelist/IssueListAppTest.java
@@ -4,18 +4,22 @@ import org.junit.Test;
 
 public class IssueListAppTest {
 
+	private static final String ISSUES_FILE = "--issues-file";
+	private static final String REPORT_TYPE = "--report-type";
+	private static final String REPORT_FILE = "--report-file";
+
 	@Test
 	public void main() throws Exception {
 		String filePath = "src/test/resources/issuelist.csv";
 		String outputFile = "target/issue-list.html";
-		IssueListApp.main(new String[]{filePath, outputFile});
+		IssueListApp.main(new String[]{ ISSUES_FILE, filePath, REPORT_TYPE, "xyz", REPORT_FILE, outputFile });
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void mainNonExistingFile() throws Exception {
 		String filePath = "src/test/resources/issuelistNonExisting.csv";
 		String outputFile = "target/issue-list.html";
-		IssueListApp.main(new String[]{filePath, outputFile});
+		IssueListApp.main(new String[]{ ISSUES_FILE, filePath, REPORT_TYPE, "xyz", REPORT_FILE, outputFile });
 	}
 	
 	//filePath is geen file (maar een map)
@@ -23,7 +27,7 @@ public class IssueListAppTest {
 	public void mainNotFile() throws Exception {
 		String outputFile = "target/issue-list.html";
 		String filePath = "src/test/resources";
-		IssueListApp.main(new String[]{filePath, outputFile});
+		IssueListApp.main(new String[]{ ISSUES_FILE, filePath, REPORT_TYPE, "xyz", REPORT_FILE, outputFile });
 	}
 	
 }
